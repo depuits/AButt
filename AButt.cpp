527aca93099cf4e158633d16634f4c744de3c4e1
depuits <colmanjoeri@msn.com>
2018-04-06 16:20:30 +0200

    update interface (wip)

--- AButt.cpp
+++ AButt.cpp
@@ -1,33 +1,40 @@
 #include "Arduino.h"
 #include "AButt.h"
 
-AButt::AButt(int pin, bool intPullup, bool isDigital, bool inv, int debounce){
-	_pin = pin;
-	_intPullup = intPullup;
-	_isDigital = isDigital;
-	_inv = inv;
-	_debounce = debounce;
-	if(_intPullup){
+AButt::AButt(int pin, int debounce = 50, bool inverted = false, bool intPullup = true, bool isDigital = true):
+	_pin(pin),
+	_debounce(debounce),
+	_inverted(inverted),
+	_isDigital(isDigital),
+
+	_isPressed(false),
+	_isHold(false),
+	_wasPressed(false),
+	_wasHeld(false),
+	_lastState(false),
+	_lastHoldState(false),
+	_startLastPress(0),
+	_startLastPressInc(0),
+	_startLastRelease(0),
+	_clickDelay(200),
+	_holdDelay(500),
+
+	_clickCallback(nullptr),
+	_holdStartCallback(nullptr),
+	_holdEndCallback(nullptr)
+{
+	if(intPullup) {
 		pinMode(_pin, INPUT_PULLUP);
-	}else{
+	} else {
 		pinMode(_pin, INPUT);
 	}
 }
-/*
-void AButt::pressed(){
-  digitalWrite(13, HIGH);  
-}
 
-void AButt::released(){
-  digitalWrite(13, LOW);
-}
-*/
-
-void AButt::setPosFlank(void (*callback)()){
+void AButt::setPosFlank(void (*callback)()) {
 	_posFlankCallback = callback;
 }
 
-void AButt::setNegFlank(void (*callback)()){
+void AButt::setNegFlank(void (*callback)()) {
 	_negFlankCallback = callback;
 }
 
@@ -36,54 +43,45 @@ void AButt::setHold(unsigned long time, void (*callback)()){
 	_holdCallback = callback;
 }
 
-bool AButt::getState(){
+bool AButt::getState() {
 	return _isPressed;
 }
 
-bool AButt::getDirectState(){
+bool AButt::getDirectState() {
 	bool state;
-	if(_isDigital){
+	if(_isDigital) {
 		state = digitalRead(_pin);
-	}else{
-		state = analogRead(_pin)>512;
+	} else {
+		state = analogRead(_pin) > 512;
 	}
 
-	if(_inv){
+	if(_inverted) {
 		state = !state;
 	}
 	
 	return state;
 }
 
-void AButt::update(){
+void AButt::update() {
 	//Check the real button state
-	bool state;
-	if(_isDigital){
-		state = digitalRead(_pin);
-	}else{
-		state = analogRead(_pin)>512;
-	}
-
-	if(_inv){
-		state = !state;
-	}
+	bool state = getDirectState();
 	
 	//Manage _isPressed var
-	if(state){
-		if(!_lastState){//potential new btn press
+	if(state) {
+		if(!_lastState) {//potential new btn press
 			_startLastPress = millis();
-		}else{
-			if(millis()-_startLastPress >= _debounce){
+		} else {
+			if(millis()-_startLastPress >= _debounce) {
 				_isPressed = true; //keeps setting the _isPressed to true as long as the button is pressed for longer then de-bounce 
 			}
 		}
 	}
 	
-	if(!state){
-		if(_lastState){//potential new btn release
+	if(!state) {
+		if(_lastState) {//potential new btn release
 			_startLastRelease = millis();
-		}else{
-			if(millis()-_startLastRelease >= _debounce){
+		} else {
+			if(millis()-_startLastRelease >= _debounce) {
 				_isPressed = false;
 			}
 		}
@@ -91,8 +89,8 @@ void AButt::update(){
 	_lastState = state;
 	
 	//Check for pos flank
-	if(_isPressed && !_wasPressed){
-		if(_posFlankCallback){//if no function is assigned yet, do nothing
+	if(_isPressed && !_wasPressed) {
+		if(_posFlankCallback) { //if no function is assigned yet, do nothing
 			(*_posFlankCallback)();
 		}
 		_wasPressed = _isPressed;
@@ -100,36 +98,36 @@ void AButt::update(){
 
 	
 	//Check for neg flank
-	if(!_isPressed && _wasPressed){
-		if(_negFlankCallback){//if no function is assigned yet, do nothing
+	if(!_isPressed && _wasPressed) {
+		if(_negFlankCallback) { //if no function is assigned yet, do nothing
 			(*_negFlankCallback)();
 		}
 		_wasPressed = _isPressed;
 	}
 	
 	//Check for hold (including the de-bounce)
-	if(_isPressed){
-		if(!_lastHoldState){//potential new hold
+	if(_isPressed) {
+		if(!_lastHoldState) {//potential new hold
 			_startLastPressInc = millis();
-		}else{
-			if(millis()-_startLastPressInc >= _holdTime){
+		} else {
+			if(millis()-_startLastPressInc >= _holdTime) {
 				_isHold = true; //keeps setting the _isPressed to true as long as the button is pressed for longer then de-bounce 
 			}
 		}
-	}else{
+	} else {
 		_isHold = false;
 	}
 	_lastHoldState = _isPressed;
 	
 	//Check for pos hold flank
-	if(_isHold && !_wasHeld){
-		if(_holdCallback){//if no function is assigned yet, do nothing
+	if(_isHold && !_wasHeld) {
+		if(_holdCallback) { //if no function is assigned yet, do nothing
 			(*_holdCallback)();
 		}
 		_wasHeld = _isHold;
 	}
 	//Check for neg hold flank (response not yet implemented)
-	if(!_isHold && _wasHeld){
+	if(!_isHold && _wasHeld) {
 		_wasHeld = _isHold;
 	}
 }
--- AButt.h
+++ AButt.h
@@ -1,28 +1,36 @@
-/*
-  
-*/
 #ifndef AButt_h
 #define AButt_h
 
-#include "Arduino.h"
-
 class AButt
 {
-  public:
-    AButt(int pin, bool intPullup, bool isDigital, bool inv, int debounce);
+public:
+	AButt(int pin, int debounce = 50, bool inverted = false, bool intPullup = true, bool isDigital = true);
+
 	void update();
-	void setPosFlank(void (*callback)());
-	void setNegFlank(void (*callback)());
-	void setHold(unsigned long time, void (*callback)()) ;//when button is held for x time execute
-	bool getState(); //return state including de-bounce 
-	bool getDirectState(); //return state excluding de-bounce, including inversion etc
-	
-  private:
-    int _pin; //Pin conected to the button
-	bool _intPullup; //Set high if internal pullup is used
-	bool _isDigital; //Is the pin digital, digital read on analog pins can cause problems sometime
-	bool _inv; //if inv is true, the button is pressed when there is 0V on Pin
+
+	void onClick(void (*callback)(int));
+	void onHold(void (*start)(), void (*end)());
+
+	//return state including de-bounce
+	bool getState();
+
+	//return state excluding de-bounce, including inversion etc
+	bool getDirectState(); 
+
+	// time before a click is registered as a new sequense
+	void setClickDelay(unsigned int delay);
+	int getClickDelay();
+
+	// time before a click becomes a hold
+	void setHoldDelay(unsigned int delay);
+	int getHoldDelay();
+
+private:
+	int _pin; //Pin conected to the button
 	int _debounce; //nr of ms the btn needs to stay pressed
+	bool _inverted; //if inv is true, the button is pressed when there is 0V on Pin
+	bool _isDigital; //Is the pin digital, digital read on analog pins can cause problems sometime
+
 	bool _isPressed; //is btn pressed. This INCLUDES the debounce compensation.
 	bool _isHold;// is the btn beeing hold?
 	bool _wasPressed; //was btn pressed. This INCLUDES the debounce compensation.
@@ -32,11 +40,13 @@ class AButt
 	unsigned long _startLastPress; //start last btn use
 	unsigned long _startLastPressInc; //start last btn use, including de-bounce
 	unsigned long _startLastRelease; //start last btn release
-	unsigned long _holdTime; //start last btn release
-	void (*_posFlankCallback)(void);
-	void (*_negFlankCallback)(void);
-	void (*_holdCallback)(void);
-	
+
+	unsigned int _clickDelay; //time before a click is registered as a new sequense
+	unsigned int _holdDelay; //time before a click becomes a hold
+
+	void (*_clickCallback)(int);
+	void (*_holdStartCallback)();
+	void (*_holdEndCallback)();
 };
 
 #endif	
